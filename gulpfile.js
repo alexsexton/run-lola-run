1572e8ddf3829f14d62bc4bd8310253ee71af4c7
Alex Sexton <alex@alexsexton.cc>
2021-12-14 11:59:30 +0000

    Update: so the watch task works

--- gulpfile.js
+++ gulpfile.js
@@ -29,12 +29,16 @@ const sass = gulpsass(dartsass)
 
 var folder = {
   src: 'assets/',
-  build: 'static/'
+  build: 'static/',
 }
 
 // Clean
 gulp.task('clean', function () {
-  return del(folder.build + 'styles.css', folder.build + 'main.js', folder.build + 'main.min.js')
+  return del(
+    folder.build + 'styles.css',
+    folder.build + 'main.js',
+    folder.build + 'main.min.js'
+  )
 })
 
 // gulp.task('clean_js', function () {
@@ -44,7 +48,8 @@ gulp.task('clean', function () {
 // image processing
 gulp.task('images', function () {
   const out = folder.build
-  return gulp.src(folder.src + 'images/*')
+  return gulp
+    .src(folder.src + 'images/*')
     .pipe(newer(out))
     .pipe(imagemin({ optimizationLevel: 5 }))
     .pipe(gulp.dest(out))
@@ -53,74 +58,79 @@ gulp.task('images', function () {
 // SVG min
 gulp.task('svgmin', function () {
   const out = folder.build
-  return gulp.src(folder.src + 'images/*')
+  return gulp
+    .src(folder.src + 'images/*')
     .pipe(svgmin())
     .pipe(gulp.dest(out))
 })
 
 // CSS processing
-gulp.task('css', gulp.series('clean', 'images', function () {
-  const postCssOpts = [
-    assets({ loadPaths: ['assets/'] }),
-    autoprefixer
-  ]
-  postCssOpts.push(cssnano)
+gulp.task(
+  'css',
+  gulp.series('clean', 'images', function () {
+    const postCssOpts = [assets({ loadPaths: ['assets/'] }), autoprefixer]
+    postCssOpts.push(cssnano)
 
-  return gulp.src(folder.src + 'scss/main.scss')
-    .pipe(sass({
-      outputStyle: 'nested',
-      imagePath: 'images/',
-      precision: 3,
-      errLogToConsole: true
-    }))
-    .pipe(postcss(postCssOpts))
-    .pipe(gulp.dest(folder.build))
-}))
+    return gulp
+      .src(folder.src + 'scss/main.scss')
+      .pipe(
+        sass({
+          imagePath: 'images/',
+          precision: 3,
+          errLogToConsole: true,
+        })
+      )
+      .pipe(postcss(postCssOpts))
+      .pipe(gulp.dest(folder.build))
+  })
+)
 
 // Babel processing
-gulp.task('babel', gulp.series(function (done) {
-  gulp.src([
-    folder.src + 'js/lib/*',
-    folder.src + 'js/*'
-  ])
-    .pipe(babel())
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(folder.build))
-  done()
-}))
+gulp.task(
+  'babel',
+  gulp.series(function (done) {
+    gulp
+      .src([folder.src + 'js/lib/*', folder.src + 'js/*'])
+      .pipe(babel())
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest(folder.build))
+    done()
+  })
+)
 
 // JavaScript processing
-gulp.task('js', gulp.series('babel', function () {
-  let jsbuild = gulp.src([
-    folder.src + 'js/lib/*',
-    folder.src + 'js/main.js'
-  ]) // <- Multiple files need to go in an array
-    .pipe(babel())
-    .pipe(concat('main.min.js'))
+gulp.task(
+  'js',
+  gulp.series('babel', function () {
+    let jsbuild = gulp
+      .src([folder.src + 'js/lib/*', folder.src + 'js/main.js']) // <- Multiple files need to go in an array
+      .pipe(babel())
+      .pipe(concat('main.min.js'))
 
-  jsbuild = jsbuild
-    .pipe(stripdebug())
-    .pipe(terser())
+    jsbuild = jsbuild.pipe(stripdebug()).pipe(terser())
 
-  return jsbuild.pipe(gulp.dest(folder.build))
-}))
+    return jsbuild.pipe(gulp.dest(folder.build))
+  })
+)
 
 // run all tasks
 gulp.task('run', gulp.series('clean', 'images', 'css', 'babel', 'js'))
 
 // watch for changes
-gulp.task('watch', gulp.series('run', function () {
-  // image changes
-  gulp.watch(folder.src + 'images/**/*', gulp.series('images'))
-  // javascript changes
-  gulp.watch(folder.src + 'js/**/*', gulp.series('babel', 'js'))
-  // css changes
-  gulp.watch(folder.src + 'scss/**/*', gulp.series('css'))
-}))
+gulp.task(
+  'watch',
+  gulp.parallel('run', function () {
+    // image changes
+    gulp.watch(folder.src + 'images/**/*', gulp.series('images'))
+    // javascript changes
+    gulp.watch(folder.src + 'js/**/*', gulp.series('babel', 'js'))
+    // css changes
+    gulp.watch(folder.src + 'scss/**/*', gulp.series('css'))
+  })
+)
 
 // default task
 gulp.task('default', gulp.series('run'))
-
 
 // default task
 exports.default = gulp.series(exports.build, exports.watch)
